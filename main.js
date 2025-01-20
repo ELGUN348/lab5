008f5240861485a4850134fd7eb2668a345dfe58
use Name <you@example.com>
2025-01-20 08:20:03 -0800

    helletdim

---
+++ main.js
@@ -0,0 +1,20 @@
+let accordion=document.getElementsByClassName('contentBx');
+for(i=0;i<accordion.length;i++){
+    accordion[i].addEventListener('click',function(){
+        this.classList.toggle('active')
+    })
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
